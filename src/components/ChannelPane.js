e30aa2321ebc0b5335fbcc9a834851ba648931cb
Jacob O'Quinn <jacob.oquinn374@gmail.com>
2021-11-17 04:31:55 -0500

    Added Channel Pane Files

---
+++ src/components/ChannelPane.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import styled from 'styled-components'
+import { func, array } from 'prop-types';
+import "./styles/channelToggle.css"
+
+export const ChannelPane = (props) => {
+
+    const {channels, handleChannelToggle} = props;
+
+    const ChannelToggle = (index) => {
+        return( 
+            <span>
+                <label className="switch">
+                    <input type="checkbox" onChange={() => handleChannelToggle(index)}/>
+                    <span className="slider"/>
+                </label>
+            </span>
+        );
+    }
+
+    const renderChannel = (channel, index) => {
+        return (
+            <ChannelCell>
+                <ChannelCellContent>
+                    <span>
+                        {channel.channelName}
+                    </span>
+                    
+                    <ChannelToggle index={index}/>
+                </ChannelCellContent>
+            </ChannelCell>
+        );
+    }
+
+    return (
+        <div>
+            {channels.map(renderChannel)}
+        </div>
+    );
+} 
+ChannelPane.propTypes = {
+    channels: array,
+    handleInputChange: func.isRequired,
+};
+
+const ChannelList = styled.div`
+    /* Auto Layout */
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px;
+
+    position: absolute;
+    width: 200px;
+    height: 230px;
+    left: 1080px;
+    top: 48px;
+`
+
+const ChannelCell = styled.div`
+    position: static;
+    width: 200px;
+    height: 32px;
+    left: 0px;
+    top: 0px;
+
+
+    /* Inside Auto Layout */
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 0px 0px;
+`
+const ChannelCellContent = styled.div`
+    /* Auto Layout */
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px;
+    
+    width: 180px;
+    height: 12px;
+    left: 8px;
+    top: 8px;
+    
+`
