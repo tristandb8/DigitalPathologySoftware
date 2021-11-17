ad0f1ff718f1618a366dcc25d33dea3e38f23bab
SPVTNIK-ONLINE <monarch.michael.e@gmail.com>
2021-11-16 23:48:50 -0500

    Made current canvas a component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { TiffModel } from './utils/TiffModel'
 import styled from 'styled-components'
 import './App.css'
-import {TransformWrapper, TransformComponent} from 'react-zoom-pan-pinch'
+import PanZoomCanvas from './components/PanZoomCanvas'
 const { ipcRenderer } = window.require('electron')
 
 class App extends Component {
@@ -12,9 +12,6 @@ class App extends Component {
       loadedFileType: null,
       loadedFile: null
     }
-
-    this.canvasRef = React.createRef(null)
-    this.viewRef = React.createRef(null)
   }
   
   componentDidMount() {
@@ -22,8 +19,7 @@ class App extends Component {
       let file
 
       if (fileContent.type === 'tiff') {
-        const tiffFile = new TiffModel(fileContent.data)
-        file = tiffFile
+        file = new TiffModel(fileContent.data)
       } else if (fileContent.type === 'image') {
         file = fileContent.data
       }
@@ -34,51 +30,16 @@ class App extends Component {
       })
     })
   }
-
-  componentDidUpdate() {
-    const canvas = this.canvasRef.current
-    const ctx = canvas.getContext('2d')
-    const file = this.state.loadedFile
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (this.state.loadedFileType === 'tiff')
-    {
-      canvas.width = file.width
-      canvas.height = file.height
-
-      // Ideally this would be changeable via dropdown
-      ctx.globalCompositeOperation = 'color'
-      
-      file.getBitmap().then(function(layers) {
-        console.log(`drawing ${layers.length} layers`)
-        for (let i = 0; i < layers.length; i++) {
-          ctx.drawImage(layers[i], 0, 0)
-        }
-      })
-    } else {
-      const img = new Image()
-      img.src = file
-      canvas.width = 300
-      canvas.height = 150
-      ctx.drawImage(img, 0, 0)
-      console.log(canvas.height)
-    }
-
-    this.viewRef.current.resetTransform(0, 'easeOut')
-    this.viewRef.current.centerView(undefined, 0, 'easeOut')
-  }
   
   render() {
+    console.log('passing:')
+    console.log(this.state)
     return (
       <div className='App'>
         <Split>
           <Pane></Pane>
           <Display>
-            <TransformWrapper ref={this.viewRef} initialScale={1}>
-              <TransformComponent wrapperStyle={{width: '100%', height: '100%'}}>
-                <canvas ref={this.canvasRef} style={{imageRendering: 'pixelated', outline: 'red 1px solid'}}></canvas>
-              </TransformComponent>
-            </TransformWrapper>
+            <PanZoomCanvas file={this.state}/>
           </Display>
           <Pane></Pane>
         </Split>
--- src/components/PanZoomCanvas.js
+++ src/components/PanZoomCanvas.js
@@ -1,226 +1,65 @@
-import {
-  useCallback,
-  createContext,
-  useRef,
-  useLayoutEffect,
-  useEffect,
-  useContext,
-  useState
-} from 'react'
-import useEventListener from '../utils/useEventListener'
-import * as pointUtils from '../utils/pointUtils'
+import React from 'react'
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 
-const MIN_SCALE = 0.5
-const MAX_SCALE = 3
-
-export function usePan() {
-  const [panState, setPanState] = useState(pointUtils.ORIGIN)
-  const lastPointRef = useRef(pointUtils.ORIGIN)
-
-  const pan = useCallback((e) => {
-    const lastPoint = lastPointRef.current
-    const point = {x: e.pageX, y: e.pageY}
-    lastPointRef.current = point
-
-    setPanState(panState => {
-      const delta = pointUtils.diff(lastPoint, point)
-      const offset = pointUtils.sum(panState, delta)
-      return offset
-    })
-  }, [])
-
-  const endPan = useCallback(() => {
-    document.removeEventListener('mousemove', pan)
-    document.removeEventListener('mouseup', endPan)
-  }, [pan])
-
-  const startPan = useCallback(
-    (e) => {
-      document.addEventListener('mousemove', pan)
-      document.addEventListener('mouseup', endPan)
-      lastPointRef.current = {x: e.pageX, y: e.pageY}
-    },
-    [pan, endPan]
-  )
-
-  return [panState, startPan]
-}
-
-export function useScale(ref) {
-  const [scale, setScale] = useState(1)
-
-  const updateScale = ({direction, interval}) => {
-    setScale(currentScale => {
-      let scale
-
-      // Adjust up to or down to the maximum or minimum scale levels by `interval`.
-      if (direction === 'up' && currentScale + interval < MAX_SCALE) {
-        scale = currentScale + interval
-      } else if (direction === 'up') {
-        scale = MAX_SCALE
-      } else if (direction === 'down' && currentScale - interval > MIN_SCALE) {
-        scale = currentScale - interval
-      } else if (direction === 'down') {
-        scale = MIN_SCALE
-      } else {
-        scale = currentScale
-      }
-
-      return scale
-    })
+export default class PanZoomCanvas extends React.Component {
+  constructor(props) {
+    console.log(props)
+    super(props)
+    this.canvasRef = React.createRef(null)
+    this.viewRef = React.createRef(null)
+    this.state = {
+      file: null
+    }
   }
 
-  // Set up an event listener such that on `wheel`, we call `updateScale`.
-  useEventListener(ref, 'wheel', e => {
-    e.preventDefault()
-
-    updateScale({
-      direction: e.deltaY > 0 ? 'up' : 'down',
-      interval: 0.1
-    })
-  })
-
-  return scale
-}
-
-export function useLast(value) {
-  const ref = useRef(value)
+  updateCanvas() {
+    const canvas = this.canvasRef.current
+    const ctx = canvas.getContext('2d')
+    const loadedFile = this.props.file.loadedFile
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
   
-  useEffect(() => {
-    ref.current = value
-  }, [value])
-
-  return ref.current
-}
-
-export function useMousePos(ref) {
-  const mousePos = useRef(pointUtils.ORIGIN)
-
-  useEventListener(ref, 'mousemove', e => {
-    e.preventDefault()
-    mousePos.current = e
-  })
-
-  return mousePos
-}
-
-export const TrackingExample = () => {
-  const [buffer, setBuffer] = useState(pointUtils.ORIGIN)
-  const ref = useRef(null)
-  const [offset, startPan] = usePan()
-  const scale = useScale(ref)
-  const mousePosRef = useMousePos(ref)
-  const lastOffset = useLast(offset)
-  const lastScale = useLast(scale)
-  const delta = pointUtils.diff(offset, lastOffset)
-  const adjustedOffset = useRef(pointUtils.sum(offset, delta))
-
-  if (lastScale === scale) {
-    adjustedOffset.current = pointUtils.sum(
-      adjustedOffset.current,
-      pointUtils.scale(delta, scale)
-    )
-  } else {
-    const lastMouse = pointUtils.scale(mousePosRef.current, lastScale)
-    const newMouse = pointUtils.scale(mousePosRef.current, scale)
-    const mouseOffset = pointUtils.diff(lastMouse, newMouse)
-    adjustedOffset.current = pointUtils.sum(adjustedOffset.current, mouseOffset)
+    if (this.props.file.loadedFileType === 'tiff')
+    {
+      canvas.width = loadedFile.width
+      canvas.height = loadedFile.height
+  
+      // Ideally this would be changeable via dropdown
+      ctx.globalCompositeOperation = 'color'
+      
+      loadedFile.getBitmap().then(function(layers) {
+        // console.log(`drawing ${layers.length} layers`)
+        for (let i = 0; i < layers.length; i++) {
+          ctx.drawImage(layers[i], 0, 0)
+        }
+      })
+    } else {
+      const img = new Image()
+      img.src = loadedFile
+      canvas.width = 300
+      canvas.height = 150
+      ctx.drawImage(img, 0, 0)
+      console.log(canvas.height)
+    }
+  
+    this.viewRef.current.resetTransform(0, 'easeOut')
+    this.viewRef.current.centerView(undefined, 0, 'easeOut')
+  }
+  
+  componentDidUpdate(oldProps) {
+    if (oldProps.file != this.props.file) {
+      console.log('fetched')
+      console.log(this.props.file)
+      this.updateCanvas()
+    }
   }
 
-  useLayoutEffect(() => {
-    const height = ref.current?.clientHeight ?? 0
-    const width = ref.current?.clientWidth ?? 0
-
-    setBuffer({
-      x: (width - width / scale) / 2,
-      y: (height - height / scale) / 2
-    })
-  }, [scale, setBuffer])
-
-  return (
-    <div ref={ref} onMouseDown={startPan} style={{position: 'relative', display: '100%', height: '100%'}}>
-      <div
-        style={{
-          backgroundImage: 'url(https://jclem.nyc3.cdn.digitaloceanspaces.com/pan-zoom-canvas-react/grid.svg)',
-          transform: `scale(${scale})`,
-          display: 'block',
-          backgroundPosition: `${-adjustedOffset.current.x}px ${-adjustedOffset
-            .current.y}px`,
-          position: 'absolute',
-          bottom: buffer.y,
-          left: buffer.x,
-          right: buffer.x,
-          top: buffer.y
-        }}
-      ></div>
-    </div>
-  )
-}
-
-export const CanvasContext = createContext({})
-export function CanvasProvider(props) {
-  const [buffer, setBuffer] = useState(pointUtils.ORIGIN)
-  const ref = useRef(null)
-  const [offset, startPan] = usePan()
-  const scale = useScale(ref)
-  const mousePosRef = useMousePos(ref)
-  const lastOffset = useLast(offset)
-  const lastScale = useLast(scale)
-  const delta = pointUtils.diff(offset, lastOffset)
-  const adjustedOffset = useRef(pointUtils.sum(offset, delta))
-
-  if (lastScale === scale) {
-    adjustedOffset.current = pointUtils.sum(
-      adjustedOffset.current,
-      pointUtils.scale(delta, scale)
+  render() {
+    return (
+      <TransformWrapper ref={this.viewRef} initialScale={1}>
+        <TransformComponent wrapperStyle={{width: '100%', height: '100%'}}>
+          <canvas ref={this.canvasRef} style={{imageRendering: 'pixelated', outline: 'red 1px solid'}}></canvas>
+        </TransformComponent>
+      </TransformWrapper>
     )
-  } else {
-    const lastMouse = pointUtils.scale(mousePosRef.current, lastScale)
-    const newMouse = pointUtils.scale(mousePosRef.current, scale)
-    const mouseOffset = pointUtils.diff(lastMouse, newMouse)
-    adjustedOffset.current = pointUtils.sum(adjustedOffset.current, mouseOffset)
   }
-
-  useLayoutEffect(() => {
-    const height = ref.current?.clientHeight ?? 0
-    const width = ref.current?.clientWidth ?? 0
-
-    setBuffer({
-      x: (width - width / scale) / 2,
-      y: (height - height / scale) / 2
-    })
-  }, [scale, setBuffer])
-
-  return (
-    <CanvasContext.Provider
-      value={{
-        offset: adjustedOffset.current,
-        scale,
-        buffer
-      }}
-    >
-      <div ref={ref} onMouseDown={startPan} style={{position: 'relative', width: '100%', height: '100%'}}>
-        {props.children}
-      </div>
-    </CanvasContext.Provider>
-  )
-}
-
-export function GridBackground() {
-  const {offset, buffer, scale} = useContext(CanvasContext)
-
-  return (
-    <div
-      style={{
-        backgroundImage: 'url(https://jclem.nyc3.cdn.digitaloceanspaces.com/pan-zoom-canvas-react/grid.svg)',
-        display: 'block',
-        transform: `scale(${scale})`,
-        backgroundPosition: `${-offset.x}px ${-offset.y}px`,
-        position: 'absolute',
-        bottom: buffer.y,
-        left: buffer.x,
-        right: buffer.x,
-        top: buffer.y
-      }}/>
-  )
 }
