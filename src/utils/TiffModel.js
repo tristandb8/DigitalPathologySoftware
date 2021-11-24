a3eabca5fbaea97805c2d1d8c7e446d207b8c006
SPVTNIK-ONLINE <monarch.michael.e@gmail.com>
2021-11-23 19:01:28 -0500

    Fixed threshold rendering

--- src/utils/TiffModel.js
+++ src/utils/TiffModel.js
@@ -86,7 +86,8 @@ export const getChannelImageData = (t, index) => {
   for (let i = 0; i < data.length; i++) {
     const lowThresh = channel.max * channel.threshold[0] / 100
     const highThresh = channel.max * channel.threshold[1] / 100
-    const threshVal = (data[i] <= highThresh && data[i] >= lowThresh) ? (data[i] - lowThresh) / highThresh: 0
+    // const threshVal = (data[i] <= highThresh && data[i] >= lowThresh) ? (data[i] - lowThresh) / highThresh: 0
+    const threshVal = (data[i] - lowThresh) / highThresh
     intArray[j++] = threshVal * color.r // R value
     intArray[j++] = threshVal * color.g // G value
     intArray[j++] = threshVal * color.b // B value
