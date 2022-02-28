76d93d3b0fe8609d67b17a295242ed08f8299fa5
ryan kendrick <ryanmac@ryans-MacBook-Pro.local>
2022-02-28 17:15:14 -0500

    Changing the return value back to text for python-scripts. Also doing testing in test.py so it does not take so long to run tests.

--- src/python/test.py
+++ src/python/test.py
@@ -1,2 +1,19 @@
+import json
+from json import JSONEncoder
+import numpy
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
 
 print('Hello World!')
+numpyArrayOne = numpy.array([[11, 22, 33], [44, 55, 66], [77, 88, 99]])
+numpyData = {"array": numpyArrayOne}
+# use dump() to write array into file
+encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)
+print("Printing JSON serialized NumPy array")
+print(encodedNumpyData)
