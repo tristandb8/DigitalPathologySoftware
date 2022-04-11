198304e08185bbbe296a412f8891a7b26b00f7ea
Jacob O'Quinn <jacob.oquinn374@gmail.com>
2022-04-11 19:42:19 -0400

    Revert "Get_stats function"
    This reverts commit a1fb8fc4a36069d7dd3560e2617f038b096d0f01.

--- src/python/cytoplasm.py
+++ src/python/cytoplasm.py
@@ -1,7 +1,5 @@
-import csv
 import math
-import numpy as np
-import pandas as pd
+import numpy
 import sys
 import json
 from PIL import Image
@@ -12,7 +10,7 @@ import os
 
 class NumpyArrayEncoder(JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, np.ndarray):
+        if isinstance(obj, numpy.ndarray):
             return obj.tolist()
         return JSONEncoder.default(self, obj)
 
@@ -27,7 +25,7 @@ class Nucleus(object):
 
 # 2-D array with unique values:
 args = json.load(open(sys.argv[1], 'r'))
-detections = np.asarray(args)
+detections = numpy.asarray(args)
 detections = detections.astype(int)
 print(detections.shape)
 print(detections[30][120:160])
@@ -52,6 +50,7 @@ nrows = len(arr)
 ncols = len(arr[0])
 
 sizes = []
+edges = []
 ids_detected = {0}-{0}  # set()
 
 edges = [[] for _ in range(num_nuclei)]
@@ -62,6 +61,7 @@ nuclei = []
 for r in range(nrows):
     for c in range(ncols):
         if arr[r][c] != 0 and not arr[r][c] in ids_detected:
+
             nucleus_id = arr[r][c]
             ids_detected.add(nucleus_id)
 
@@ -107,7 +107,7 @@ for r in range(nrows):
 ids_arr = detections.copy()
 
 inf = 9999
-distance = np.zeros((len(detections), len(detections[0])), dtype=float)
+distance = numpy.zeros((len(detections), len(detections[0])), dtype=float)
 for r in range(len(detections)):
     for c in range(len(detections[0])):
         distance[r][c] = inf if detections[r][c] == 0 else -1
@@ -150,8 +150,8 @@ while len(queue) > 0:
             queue.append((nr, nc, ids_arr[r][c]))
 
 # Mark cytoplasm as negative
-mask = np.ma.masked_equal(detections, 0)
-final_mask = np.where(detections == 0, ids_arr*(-1), ids_arr)
+mask = numpy.ma.masked_equal(detections, 0)
+final_mask = numpy.where(detections == 0, ids_arr*(-1), ids_arr)
 # print(final_mask.shape)
 
 # Final mask is what you want, each cyto value is negative,
@@ -187,72 +187,3 @@ with open(saveFile, 'w') as fp:
 # image_array = np.uint16(np.random.rand(200, 200) * 65535)
 # image_pil = Image.fromarray(image_array, 'I;16')
 # image_pil.save('image_pil.jp2')
-
-
-##### ------------------------------ ######
-
-def get_stats(image_path, mask, csv_output_path='../tmp/cell_analysis.csv'):
-  """
-  Calulates important statistics for the input image given its corresponding detection mask
-    and outputs it to a csv at the desired location.
-
-  Parameters:
-  -----------------
-  image_path : str, the relative file path of the original .tiff image being analyzed
-  mask : 2d_arr, 2d array where nuclei are marked with unique values 1-n (n = number of nuclei)
-    and nuclei's cytoplasm is marked with its corresponding negative value
-  csv_output_path : str, relative path the .csv should be output to (default is in tmp folder)
-  """
-
-  im = Image.open(image_path) 
-  im.load()
-
-  cyto_positions = [[] for _ in range(np.amax(mask))]
-  nuclei_positions = [[] for _ in range(np.amax(mask))]
-
-  for i in range(len(mask)):
-    for j in range(len(mask[0])):
-      val = mask[i][j]
-      if val >= 0:
-        nuclei_positions[val - 1].append((i,j))
-      else:
-        cyto_positions[val - 1].append((i,j))
-  
-  values = []
-  for channel in range(im.n_frames):
-    im.seek(channel)
-    values.append(np.array(im))
-
-  def calculate_cell(cyto_positions, nuclei_positions, values):
-    def calculate_cell_helper(values, channel):
-      nuclei_vals = [values[i][j] for i,j in nuclei_positions]
-      cyto_vals = [values[i][j] for i,j in cyto_positions]
-      cell_vals = [values[i][j] for i,j in (nuclei_positions + cyto_positions)]
-
-      return {
-              'Channel {}: Cell_Area'.format(str(channel)): len(cell_vals),
-              'Channel {}: Cell_Mean'.format(str(channel)): np.mean(cell_vals),
-              'Channel {}: Cell_Std_dev'.format(str(channel)): np.std(cell_vals), 
-              'Channel {}: Cell_Max'.format(str(channel)): np.amax(cell_vals),
-              'Channel {}: Cell_Min'.format(str(channel)): np.amin(cell_vals),
-              'Channel {}: Nucleus_Area'.format(str(channel)): len(nuclei_vals),
-              'Channel {}: Nucleus_Mean'.format(str(channel)): np.mean(nuclei_vals),
-              'Channel {}: Nucleus_Std_dev'.format(str(channel)): np.std(nuclei_vals), 
-              'Channel {}: Nucleus_Max'.format(str(channel)): np.amax(nuclei_vals),
-              'Channel {}: Nucleus_Min'.format(str(channel)): np.amin(nuclei_vals),
-              'Channel {}: Cytoplasm_Area'.format(str(channel)): len(cyto_vals),
-              'Channel {}: Cytoplasm_Mean'.format(str(channel)): np.mean(cyto_vals),
-              'Channel {}: Cytoplasm_Std_dev'.format(str(channel)): np.std(cyto_vals), 
-              'Channel {}: Cytoplasm_Max'.format(str(channel)): np.amax(cyto_vals),
-              'Channel {}: Cytoplasm_Min'.format(str(channel)): np.amin(cyto_vals)
-            }
-
-    data = [calculate_cell_helper(values[channel], channel) for channel in range(im.n_frames)]
-    return {k: v for d in data for k, v in d.items()}
-  
-  # calculate stats for every cell detected
-  stats = [calculate_cell(cyto_positions[cell], nuclei_positions[cell], values) for cell in range(len(cyto_positions))] 
-  pd.DataFrame(stats).to_csv(csv_output_path)
-
-
-# get_stats(image_path=???, mask=final_mask, csv_output_path='../tmp/tmp_analysis.csv')
